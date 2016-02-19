bbd4ac4c7a7355cb62aba27d730b8c8c054d09da
YISUSVII <jesus_arellano3@hotmail.com>
2016-02-18 20:10:44 -0600

    Quienes somos
    Agregado apartado Quienes somos con fotografias y ligas de integrantes

--- Galery.php
+++ Galery.php
@@ -13,48 +13,45 @@
 
 	
 
-<div class="container-fluid">
-	<div class="row">
-	<div class="col-md-12">
-		<?php include 'inc/footer_common.php'; ?>
-		<?php include 'inc/header.php'; ?>
-		 <div class="row">
-				<div class="col-md-4">
-					<h3 class="text-center">
-						<i class="fa fa-cogs fa-5x"></i>
-					</h3>
-					<h3 class="text-center">
-						Aplicaciones para Empresas 
-					</h3>
-					
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<?php include 'inc/footer_common.php'; ?>
+				<?php include 'inc/header.php'; ?>
+				<div class="row">
+					<div class="col-md-4">
+						<h3 class="text-center">
+							<i class="fa fa-cogs fa-5x"></i>
+						</h3>
+						<h3 class="text-center">
+							Aplicaciones para Empresas 
+						</h3>
+
+					</div>
+					<div class="col-md-4">
+
+						<h3 class="text-center">
+							<i class="fa fa-paint-brush fa-5x"></i>		
+						</h3>
+						<h3 class="text-center">
+							Diseño de Sitios Web
+						</h3>
+					</div>
+					<div class="col-md-4">
+						<h3 class="text-center">
+							<i class="fa fa-university fa-5x"></i>
+
+						</h3>
+						<h3 class="text-center">
+							Software Escolar
+						</h3>
+					</div>
+
 				</div>
-				<div class="col-md-4">
-					
-					<h3 class="text-center">
-						<i class="fa fa-paint-brush fa-5x"></i>		
-					</h3>
-					<h3 class="text-center">
-						Diseño de Sitios Web
-					</h3>
-				</div>
-				<div class="col-md-4">
-					<h3 class="text-center">
-					<i class="fa fa-university fa-5x"></i>
-						
-					</h3>
-					<h3 class="text-center">
-						Software Escolar
-					</h3>
-				</div>
+				
 
 		</div>
-		
-	
-		
 	</div>
-		
-	</div>
-</div>
 
 
 
--- detail.php
+++ detail.php
@@ -4,9 +4,9 @@
 	<title>JyJAH Quienes Somos</title>
 	<?php include 'inc/header_common.php'; ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel='stylesheet prefetch' href='http://puu.sh/kPy1N/61d933585d.css'>
-    <link rel="stylesheet" href="css/detail.css">
-    
+	<link rel='stylesheet prefetch' href='http://puu.sh/kPy1N/61d933585d.css'>
+	<link rel="stylesheet" href="css/detail.css">
+
 
 </head>
 <body>
@@ -14,52 +14,125 @@
 
 
 
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-		<?php include 'inc/footer_common.php'; ?>
-		<?php include 'inc/header.php'; ?>
-			<div class="row">
-				<div class="col-md-12">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<?php include 'inc/footer_common.php'; ?>
+				<?php include 'inc/header.php'; ?>
+				<div class="row">
+					<div class="col-md-12">
 
-				<div class="right">
-					<h4 class="randoms"></h4>
-					<h1 class="randoms"></h1>
-					<h4 class="randoms"></h4>
+						<div class="right">
+							<h4 class="randoms"></h4>
+							<h1 class="randoms"></h1>
+							<h4 class="randoms"></h4>
+						</div>
+						<div class="left">
+							<h4 class="randoms"></h4>
+							<h1 class="randoms"></h1>
+							<h4 class="randoms"></h4>
+						</div>
+
+						<script src="js/animation.js"></script>
+
+
+
+					</div>
+					<div class="row">
+						<div class="col-md-6">
+							<p>
+								J & JAH  <strong>Especialistas en Software Escolar</strong>. Aliquam eget sapien sapien. 
+								Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit.
+								Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi
+								commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, 
+								id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>
+								Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat 
+								ipsum consequat eu.</small>
+							</p>
+						</div>
+						<div class="col-md-6">
+							<img alt="Bootstrap Image Preview" src="img/team.png" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+
+							<figure class="snip1344"><img src="img/people/yisus.jpg" alt="profile-sample1" class="background"/><img src="img/people/yisus.jpg" alt="profile-sample1" class="profile"/>
+								<figcaption>
+									<h3>YISUSVII<span>CEO & .NET DEVELOPER </span></h3>
+									<div class="icons"><a href="#"><i class="ion-social-facebook-outline"></i></a><a href="#"> <i class="ion-social-github-outline"></i></a><a href="#"> <i class="ion-android-globe"></i></a></div>
+								</figcaption>
+							</figure>
+							<figure class="snip1344"><img  src="img/people/jonha.jpg" alt="profile-sample1" class="background"/><img src="img/people/jonha.jpg" alt="profile-sample1" class="profile"/>
+								<figcaption>
+									<h3>JONHA<span>CEO & CO-DIRECTOR</span></h3>
+									<div class="icons"><a href="#"><i class="ion-social-facebook-outline"></i></a><a href="#"> <i class="ion-social-github-outline"></i></a><a href="#"> <i class="ion-android-globe"></i></a></div>
+								</figcaption>
+							</figure>
+							<figure class="snip1344"><img src="img/people/gil.jpg" alt="profile-sample1" class="background"/><img src="img/people/gil.jpg" alt="profile-sample1" class="profile"/>
+								<figcaption>
+									<h3>Gilberto<span>Mean Stack Developer</span></h3>
+									<div class="icons"><a href="#"><i class="ion-social-facebook-outline"></i></a><a href="#"> <i class="ion-social-github-outline"></i></a><a href="#"> <i class="ion-android-globe"></i></a></div>
+								</figcaption>
+							</figure>
+							<figure class="snip1344"><img src="img/people/alan.jpg" alt="profile-sample1" class="background"/><img src="img/people/alan.jpg" alt="profile-sample1" class="profile"/>
+								<figcaption>
+									<h3>Neok<span>Android Developer</span></h3>
+									<div class="icons"><a href="#"><i class="ion-social-facebook-outline"></i></a><a href="#"> <i class="ion-social-github-outline"></i></a><a href="#"> <i class="ion-android-globe"></i></a></div>
+								</figcaption>
+							</figure>
+							<figure class="snip1344"><img src="img/people/saul.jpg" alt="profile-sample1" class="background"/><img src="img/people/saul.jpg" alt="profile-sample1" class="profile"/>
+								<figcaption>
+									<h3>Zack<span>FullStack Developer </span></h3>
+									<div class="icons"><a href="#"><i class="ion-social-facebook-outline"></i></a><a href="#"> <i class="ion-social-github-outline"></i></a><a href="#"> <i class="ion-android-globe"></i></a></div>
+								</figcaption>
+							</figure>
+							<figure class="snip1344"><img src="img/people/maury.jpg" alt="profile-sample1" class="background"/><img src="img/people/maury.jpg" alt="profile-sample1" class="profile"/>
+								<figcaption>
+									<h3>Maury<span>Software Architect</span></h3>
+									<div class="icons"><a href="#"><i class="ion-social-facebook-outline"></i></a><a href="#"> <i class="ion-social-github-outline"></i></a><a href="#"> <i class="ion-android-globe"></i></a></div>
+								</figcaption>
+							</figure>
+							<figure class="snip1344"><img src="img/people/miguel.jpg" alt="profile-sample1" class="background"/><img src="img/people/miguel.jpg" alt="profile-sample1" class="profile"/>
+								<figcaption>
+									<h3>Mike_Ob<span>Java Master Developer</span></h3>
+									<div class="icons"><a href="#"><i class="ion-social-facebook-outline"></i></a><a href="#"> <i class="ion-social-github-outline"></i></a><a href="#"> <i class="ion-android-globe"></i></a></div>
+								</figcaption>
+							</figure>
+							<figure class="snip1344"><img src="img/people/sam.jpg" alt="profile-sample1" class="background"/><img src="img/people/sam.jpg" alt="profile-sample1" class="profile"/>
+								<figcaption>
+									<h3>Sam<span>Web Developer</span></h3>
+									<div class="icons"><a href="#"><i class="ion-social-facebook-outline"></i></a><a href="#"> <i class="ion-social-github-outline"></i></a><a href="#"> <i class="ion-android-globe"></i></a></div>
+								</figcaption>
+							</figure>
+							<figure class="snip1344"><img src="img/people/daniel.jpg" alt="profile-sample1" class="background"/><img src="img/people/daniel.jpg" alt="profile-sample1" class="profile"/>
+								<figcaption>
+									<h3>Daniel<span>Videogame Developer</span></h3>
+									<div class="icons"><a href="#"><i class="ion-social-facebook-outline"></i></a><a href="#"> <i class="ion-social-github-outline"></i></a><a href="#"> <i class="ion-android-globe"></i></a></div>
+								</figcaption>
+							</figure>
+							<figure class="snip1344"><img src="img/people/omar.jpg" alt="profile-sample1" class="background"/><img src="img/people/omar.jpg" alt="profile-sample1" class="profile"/>
+								<figcaption>
+									<h3>Omar<span>Java Developer</span></h3>
+									<div class="icons"><a href="#"><i class="ion-social-facebook-outline"></i></a><a href="#"> <i class="ion-social-github-outline"></i></a><a href="#"> <i class="ion-android-globe"></i></a></div>
+								</figcaption>
+							</figure>
+							<figure class="snip1344"><img src="img/people/jacome.jpg" alt="profile-sample1" class="background"/><img src="img/people/jacome.jpg" alt="profile-sample1" class="profile"/>
+								<figcaption>
+									<h3>Jacome<span>BackEnd Developer</span></h3>
+									<div class="icons"><a href="#"><i class="ion-social-facebook-outline"></i></a><a href="#"> <i class="ion-social-github-outline"></i></a><a href="#"> <i class="ion-android-globe"></i></a></div>
+								</figcaption>
+							</figure>
+							<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+							<script src="js/detail.js"></script>
+
+
+						</div>
+
+					</div>
 				</div>
-				<div class="left">
-					<h4 class="randoms"></h4>
-					<h1 class="randoms"></h1>
-					<h4 class="randoms"></h4>
-				</div>
-					
-			     <script src="js/animation.js"></script>
-
-
-				
 			</div>
-			<div class="row">
-			<div class="col-md-6">
-					<p>
-						Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. 
-						Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit.
-						 Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi
-						  commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, 
-						  id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>
-						  Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat 
-						  ipsum consequat eu.</small>
-					</p>
-				</div>
-				<div class="col-md-6">
-					<img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
 
 
-
-
-</body>
-</html>
+		</body>
+		</html>
